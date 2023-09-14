0dbbd2aff842e6be10d74e6d7d91cb27c8d0ec3b
GabrielNezovic <gabriel.nezovic@gmail.com>
2023-09-14 16:07:07 +1000

    use pythonw to run server windows in background

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 @echo off
+taskkill /f /im pythonw.exe
 
-start cmd /k python flask_api.py
-start cmd /k streamlit run login.py
+start /B pythonw -m flask_api.py
+start /B pythonw -m streamlit run login.py --browser.gatherUsageStats false --client.toolbarMode minimal --theme.base dark --logger.level info --server.port 8008 --server.enableStaticServing true
