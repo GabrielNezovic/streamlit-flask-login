d77f2e4681f8f9e42733a256e1d678c37286d901
GabrielNezovic <gabriel.nezovic@gmail.com>
2023-09-14 16:12:37 +1000

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
 taskkill /f /im pythonw.exe
 
-start /B pythonw -m flask_api.py
+start /B pythonw flask_api.py
 start /B pythonw -m streamlit run login.py --browser.gatherUsageStats false --client.toolbarMode minimal --theme.base dark --logger.level info --server.port 8008 --server.enableStaticServing true
