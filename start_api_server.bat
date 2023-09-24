5642ee5b0ba4a4141e8f2aea92aee9bd28da7960
GabrielNezovic <gabriel.nezovic@gmail.com>
2023-09-24 15:25:31 +1000

    updating bat files

---
+++ start_api_server.bat
@@ -0,0 +1,13 @@
+@echo off
+:: Check if cert.pem file exists
+if not exist keys/api_cert.pem (
+    echo Generating self-signed SSL certificate...
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout keys/api_key.key -out keys/api_cert.crt -subj "/CN=your.public.ip.address"
+    if errorlevel 1 (
+        echo Failed to generate SSL certificate. Exiting.
+        exit /b 1
+    )
+    echo SSL certificate generated successfully.
+)
+
+start pythonw flask_api.py
