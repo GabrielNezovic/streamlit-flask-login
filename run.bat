d63262bfc25937d8e02647e243c336f078e4ceca
Gabriel Nezovic <138544043+GabrielNezovic@users.noreply.github.com>
2023-09-24 15:25:56 +1000

    Delete run.bat

--- run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-taskkill /f /im pythonw.exe
-
-start /B pythonw flask_api.py
-start /B pythonw -m streamlit run login.py --server.address localhost --server.port 8008 --theme.base dark --logger.level info --browser.gatherUsageStats false
